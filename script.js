8613e204546c7757cd951744679bba582586cb02
varshith-0405 <varshithgurthuri160405@gmail.com>
2025-03-04 10:29:09 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,28 @@
+let users = [];
+function showLogin() {
+  fetch("https://jsonplaceholder.typicode.com/users")
+    .then((res) => res.json())
+    .then((data) => displayUsers(data))
+    .catch((err) => console.log(err));
+  }
+
+function showHome(){
+root.innerHTML = "This is Home Page"
+}
+
+function displayUsers(data) {
+  let str = `
+  <div class='d-flex justify-content-center p-5'>
+  <div class='p-5'>
+  <h2>My Social Media</h2>
+  <p>This is the caption of the website.</p>
+  </div>
+  <div class='p-5'>
+  <select class='form-control m-3'>
+  <option value='0'>--Select User--</option>`
+  data.map((value) => {
+    str += `<option value=${value.id}>${value.name}</option>`;
+  });
+  str += `</select><p><button class='form-control m-3 btn btn-primary' onclick='showHome()'>Log In</button></p></div></div>`
+  root.innerHTML = str
+}
